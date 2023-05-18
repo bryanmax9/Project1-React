0d00e32a21a14ce9468a6ba9ff53db5ccb49f9fe
Bryan Tineo <brillant16.gcv@gmail.com>
2023-05-18 15:09:50 -0700

    More Components!!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
+  // We are storing all the data as a list of objects that will store all the necessary data
+  // Here we are Moking an API that is giving as class objects with data that then will be used in the fonrt-end
+
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  // We are giving the array of objects to the Expenses.js in order to create the ExpenseItems
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  // Adding Javascript code before returning the html
+  //Mocking Fetching data
+
+  // We are giving the date data to the component "ExpenseDate"
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">294.67$</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
